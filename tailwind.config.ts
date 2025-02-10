a705269defe725e62ab40b2b8bd00c2451b3ee2c
Thomas Arbaugh <151469077+TomArbaugh@users.noreply.github.com>
2025-02-10 12:18:53 -0800

    building new GUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shrinkMove: {
+          "0%": { transform: "scale(1) translate(0, 0)", opacity: "1" },
+          "100%": { transform: "scale(0.5) translate(-100%, -100%)", opacity: "1" },
+        },
+        shrinkOnly: {
+          "0%": { transform: "scale(1)", opacity: "1" },
+          "100%": { transform: "scale(0.5)", opacity: "1" },
+        },
+      },
+      animation: {
+        shrinkMove: "shrinkMove 1s ease-in-out forwards",
+        shrinkOnly: "shrinkOnly 1s ease-in-out forwards",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
