d5d75fbdb272ddde7ea0524d945a89720a3a2981
Thomas Arbaugh <151469077+TomArbaugh@users.noreply.github.com>
2025-02-10 17:41:29 -0800

    animated entrance

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { useEffect, useState } from "react";
 
 import { useOptionalUser } from "~/utils";
 
@@ -7,14 +8,30 @@ export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
   const user = useOptionalUser();
+  const [isAtTop, setIsAtTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsAtTop(window.scrollY === 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
 
-<div className="relative sm:pb-16 sm:pt-8 w-full">
+<div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           
-          <div className="fixed top-4 left-4 right-4 flex items-center justify-between z-50">
-           <div className="flex items-center space-x-4">
+          <div className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 z-50 bg-[color:rgba(254,204,27,0.5)] transition-all duration-300 ${
+              isAtTop ? "h-[50vh]" : "h-[10vh]"
+            }`}>
+           <div className={`absolute transition-all duration-300 ${
+                isAtTop
+                  ? "left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-[4]"
+                  : "left-4 top-4 transform -translate-x-0 -translate-y-0 scale-100"
+              } flex items-center space-x-4`}>
             <img src="/icons/logo.svg" className="w-12 h-12" />
             <img src="/soggynotesrecords.svg" className="w-48" />
             </div>
@@ -30,7 +47,7 @@ export default function Index() {
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
                       to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                      className="flex items-center justify-center rounded-md border border-transparent bg-blue-500 px-4 py-3 text-base font-medium text-yellow-50 shadow-sm hover:bg-yellow-50 sm:px-8"
                     >
                       Sign up
                     </Link>
@@ -43,12 +60,14 @@ export default function Index() {
                   </div>
                 )}
             </div>
-            
+   
           </div>
         </div>
       </div>
       
-
+      <div className="h-[300vh] bg-gray-100 flex justify-center items-center">
+        <p className="text-3xl">Scroll Down!</p>
+      </div>
     </main>
 
   );
